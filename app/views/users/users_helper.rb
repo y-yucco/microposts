f2171caff71e4757d4a9c6bb614791c37ddc0626
yukako yamaoka <y.yucco-de_tte_pc@outlook.jp>
2017-03-08 07:30:16 +0000

    add sign sign up amd user detail

--- app/views/users/users_helper.rb
+++ app/views/users/users_helper.rb
@@ -1,4 +1,4 @@
-module ApplicationHelper
+module UsersHelper
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
