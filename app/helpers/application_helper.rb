aeeecad31283e59942567ffd972df426a90dada5
yukako yamaoka <y.yucco-de_tte_pc@outlook.jp>
2017-03-13 04:41:16 +0000

    user-profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,3 +6,17 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
+
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+end
